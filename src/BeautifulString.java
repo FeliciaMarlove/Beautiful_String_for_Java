f07fe970585b38881c0d741de51c4672a18b15da
FeliciaMarlove <missflorencemary@hotmail.com>
2020-10-14 16:19:48 +0200

    Changing indent for inner objects

--- src/BeautifulString.java
+++ src/BeautifulString.java
@@ -5,6 +5,7 @@ public abstract class BeautifulString<T> {
     private static final String get = Suffixes.GET.get();
     private static final String has = Suffixes.HAS.get();
     private static final String is = Suffixes.IS.get();
+    private static int level = 0;
 
     public static <T> void show(T objectToOutput) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method[] methods = objectToOutput.getClass().getDeclaredMethods();
@@ -20,47 +21,53 @@ public abstract class BeautifulString<T> {
                 }
             }
         }
+        printTabs();
         System.out.println("(the object is of type " + objectToOutput.getClass().getName() + ")");
-        System.out.println("-------------------------------------------------------\n" +
-                "Information automatically output with BeautifulString.\n" +
-                "Please find original source code at: https://github.com/FeliciaMarlove/Beautiful_String_for_Java\n" +
-                "Thank you for reporting any bug or improvement ideas.");
+        --level;
     }
 
     private static <T> void printBeautifully(Method m, T o, String prefix) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method retrievedMethod = o.getClass().getMethod(m.getName());
         String[] words = m.getName().substring(prefix.length()).split("(?=\\p{Upper})");
+        printTabs();
         Arrays.stream(words).forEach(s -> System.out.print(s + " "));
         String returnType = retrievedMethod.getReturnType().getSimpleName();
         process(returnType, retrievedMethod, o);
     }
 
     private static <T> void process(String returnType, Method method, T o) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        String outputPrefix = ": \t{" + returnType + "} ";
 
         T data;
 
         if (returnType.endsWith("[]")) {
-            //TODO further cases to test: multidimensional arrays, heterogeneous arrays
+            //TODO further cases to test: multidimensional arrays, heterogeneous arrays, Objects arrays
             data = (T) getArrayWrapped(returnType, method.invoke(o));
         } else {
             data = (T) method.invoke(o);
         }
 
-        if (data instanceof Collection) {
-            System.out.println(": \t{" + returnType + "} " + data);
+        if (data == null) {
+            System.out.println(outputPrefix + data);
+        } else if (data instanceof Collection) {
+            System.out.println(outputPrefix + data);
         } else if (data instanceof Map) {
-            System.out.println(": \t{" + returnType + "} " + data);
+            System.out.println(outputPrefix + data);
         } else if (data.getClass().isArray()) {
-            System.out.println(": \t{" + returnType + "} " + Arrays.deepToString((Object[]) data));
+            System.out.println(outputPrefix + Arrays.deepToString((Object[]) data));
         } else if (data instanceof String || data instanceof Number || data instanceof Boolean) {
-            System.out.println(": \t{" + returnType + "} " + data);
+            System.out.println(outputPrefix + data);
         } else {
+            ++level;
             //TODO - consider limiting output in case of recursion! cfr java doc on deepToString
-            System.out.println(": \t{" + returnType + "} ");
+            System.out.println(outputPrefix);
+            show(data);
+
         }
     }
 
     private static <T> Object[] getArrayWrapped(String returnType, T o) {
+        if (o == null) return null;
         switch (returnType) {
             case "double[]" :
                 double[] doubleArray = (double[]) o;
@@ -122,6 +129,19 @@ public abstract class BeautifulString<T> {
                 an array of reference type objects, including wrappers
                  */
             default: return (Object[])o;
+        }
+    }
+
+    private static void printFooter() {
+        System.out.println("-------------------------------------------------------\n" +
+                "Information automatically output with BeautifulString.\n" +
+                "Please find original source code at: https://github.com/FeliciaMarlove/Beautiful_String_for_Java\n" +
+                "Thank you for reporting any bug or improvement ideas.");
+    }
+
+    private static void printTabs() {
+        for (int i = 0; i < level; i++) {
+            System.out.print("\t");
         }
     }
 
